d5f456b621805b04a26eace12c3929ebb6a8f587
Christopher Ament <chrisament45@gmail.com>
2025-01-26 19:30:44 -0500

    added model view and controller for project functionality

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const profileController = require('../controllers/profileController')
+const express = require('express');
+const router = express.Router();
+const profileController = require('../controllers/profileController'); // Ensure this path is correct
 
-router.get('/', profileController.getProfile) 
-router.put('/profileCreation', profileController.createProfile);
+router.get('/', profileController.getProfile);
+router.post('/createProject', profileController.createProject);
 
-module.exports = router
-//
+module.exports = router;
