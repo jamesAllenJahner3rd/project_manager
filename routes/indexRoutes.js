540e09ab8321778bbdc014c11f641a4bf615baa2
James Jahner <jitone1@gmail.com>
2025-02-04 19:38:36 -0600

    removed unnecessary  comment

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const indexController = require('../controllers/indexController')
 const{ensureAuth, ensureGuest} = require('../middleware/auth')
-// REmove these comments. This is commented out to it belongs in the controller and rendering the wrong thing
 
 router.get('/',indexController.getIndex) 
 
