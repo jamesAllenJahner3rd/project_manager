7b33f4798acba4cf86da23c1aabff11761b5e726
James Jahner <jitone1@gmail.com>
2025-02-04 19:30:47 -0600

    Removed a render(profile) from the indexroutes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,12 +3,7 @@ const router = express.Router()
 const indexController = require('../controllers/indexController')
 const{ensureAuth, ensureGuest} = require('../middleware/auth')
 // REmove these comments. This is commented out to it belongs in the controller and rendering the wrong thing
-// router.get("/",ensureGuest, (req, res) => {
-//     res.render("login")
-// })
-router.get("/profile",ensureAuth, function(req, res) {
-    res.render("profile")
-})
+
 router.get('/',indexController.getIndex) 
 
 module.exports = router
