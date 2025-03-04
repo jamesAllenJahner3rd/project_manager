52121d89e1d39bc5f1d9f672d48be79ebc523c71
Jasmine John <jsjohn026@gmail.com>
2025-03-04 14:00:55 -0600

    Tested if columns are saving to db

--- controllers/columnController.js
+++ controllers/columnController.js
@@ -4,11 +4,16 @@ const mongoose = require('mongoose')
 module.exports = {
   createColumn: async (req, res) => {
     try {
-      await Column.create({
-        name: req.body.column, 
+      console.log("Received a request to create a column.");
+      console.log("Request body:", req.body); // Log the request body
+      const { column } = req.body;
+      console.log("got:", column);
+      const newColumn = await Column.create({
+        name: column, 
         // documents: [],
         projectId: req.params.id
       });
+      console.log("column: ", newColumn)
       console.log("Column has been added!")
       res.redirect("/project/" + req.params.id)
     } catch (err) {
@@ -16,3 +21,26 @@ module.exports = {
     }
   }
 }
+
+// ---------------------------
+                // fetch('/columns/createColumn/67a25383dacdda28699ff3fc', {
+                //     method: 'POST',
+                //     headers: {
+                //     'Content-Type': 'application/json',
+                //     },
+                //     body: JSON.stringify({ name: columnName }), // Send the captured value
+                // })
+                // .then(response => response.json())
+                // .then(data => {
+                //     console.log('Success:', data);
+                //     // Handle the response
+                // })
+                // .catch((error) => {
+                //     console.error('Error:', error);
+                // });
+
+
+                
+                // console.log(columnContent)
+
+// ---------------------------
