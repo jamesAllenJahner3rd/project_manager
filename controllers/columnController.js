de1351dcb264744d2cb51d50b71f689f00d8ab62
Jasmine John <jsjohn026@gmail.com>
2025-03-04 14:03:27 -0600

    Tested if columns are saving to db

--- controllers/columnController.js
+++ controllers/columnController.js
@@ -21,26 +21,3 @@ module.exports = {
     }
   }
 }
-
-// ---------------------------
-                // fetch('/columns/createColumn/67a25383dacdda28699ff3fc', {
-                //     method: 'POST',
-                //     headers: {
-                //     'Content-Type': 'application/json',
-                //     },
-                //     body: JSON.stringify({ name: columnName }), // Send the captured value
-                // })
-                // .then(response => response.json())
-                // .then(data => {
-                //     console.log('Success:', data);
-                //     // Handle the response
-                // })
-                // .catch((error) => {
-                //     console.error('Error:', error);
-                // });
-
-
-                
-                // console.log(columnContent)
-
-// ---------------------------
