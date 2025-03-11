9e178a9951b7f9db96332917f76ee8e2ac8de999
James Jahner <jitone1@gmail.com>
2025-03-11 13:20:01 -0500

    Add modal for project editing functionality by chris

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const profileController = require('../controllers/profileController');
-const { ensureAuth } = require('../middleware/auth');
+const profileController = require('../controllers/profileController'); // Ensure this path is correct
+const { ensureAuth } = require('../middleware/auth')
 
-// routes for logged in user creating new project
-router.get('/', ensureAuth, profileController.getProfile);
-router.post('/project', ensureAuth, profileController.createProfile);
-// routes for making changes to existing projects
-router.get('/project/:id/edit', ensureAuth, profileController.editProject);
-router.put('/project/:id', ensureAuth, profileController.updateProject);
-router.delete('/project/:id/delete', ensureAuth, profileController.deleteProject);
-router.get('/project/:id/data', ensureAuth, profileController.getProjectData);
+
+// Route to the profile of the authenticated user
+router.get('/', ensureAuth, profileController.getProfile) 
+
+// Route to create a new profile
+router.post('/profileCreation', ensureAuth, profileController.createProfile);
+
 
 module.exports = router;
