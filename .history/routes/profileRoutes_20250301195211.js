6cebd4550de55de0667bae620c1e2d48df8b0edb
James Jahner <jitone1@gmail.com>
2025-03-11 23:30:23 -0500

    removed folder lh and history. Added a cursor focus on the create Document popup

--- .history/routes/profileRoutes_20250301195211.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-const { ensureAuth } = require('../middleware/auth');
-
-router.get('/', ensureAuth, profileController.getProfile);
-router.delete('/project/:id/delete', ensureAuth, profileController.deleteProject);
-
-module.exports = router;
