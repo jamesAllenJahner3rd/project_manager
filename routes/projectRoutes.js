15f78b87c647827d5fec52f6184de2b1bd63b9f1
James Jahner <jitone1@gmail.com>
2025-04-03 03:16:00 -0500

    project Controller - I added GetKanban; ProjectRoutes - i added Kanban:id; I created Kanban_template;profile - i updated to reflect that it doesn't send you directly to the kanban; project_template was rewritten.

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,20 +1,24 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const projectController = require('../controllers/projectController');
+const projectController = require("../controllers/projectController");
 
-router.get('/', projectController.getProjects)
-router.get('/new', projectController.newProject);
-router.post('/createProject', projectController.createProject); 
-router.get('/:id', projectController.getProject);
-router.get('/:id/edit', async (req, res) => {
-    try {
-        const project = await Project.findById(req.params.id);
-        res.render('projects/edit', { project });
-    } catch (error) {
-        res.status(500).send('Server Error');
-    }
+router.get("/", projectController.getProjects);
+router.get("/kanban:id", projectController.getKanban);
+// router.get('/CFD', projectController.getCFD);
+// router.get('/burnup', projectController.getBurnup);
+// router.get('/taskAging', projectController.getTaskAging);
+router.get("/new", projectController.newProject);
+router.post("/createProject", projectController.createProject);
+router.get("/:id", projectController.getProject);
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    res.render("projects/edit", { project });
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
 });
-router.put('/:id', projectController.updateProject);
-router.delete('/:id', projectController.deleteProject);
+router.put("/:id", projectController.updateProject);
+router.delete("/:id", projectController.deleteProject);
 
 module.exports = router;
