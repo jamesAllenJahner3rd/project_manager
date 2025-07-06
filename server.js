50744fd5408f833b93ad96e3f74ae18bd6083333
James Jahner <jitone1@gmail.com>
2025-07-06 01:30:37 -0500

    fix: update start script in package.json to use NODE_ENV directly and set trust proxy in server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.use((req, res, next) => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
+app.set('trust proxy', 1);
 //Express-session middleware
 app.use(
   session({
